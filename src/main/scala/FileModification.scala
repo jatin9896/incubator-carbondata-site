669480e310b4277ae9c2a282677bc982ecab2925
jatin <jatin.demla@knoldus.in>
2017-04-12 12:34:33 +0530

    Added Pdf Functionality

--- src/main/scala/FileModification.scala
+++ src/main/scala/FileModification.scala
@@ -15,8 +15,8 @@ class FileModification {
 
   val headerContent: String = Source.fromFile(ConfigFactory.load().getString("headerPath")).mkString
   val footerContent: String = Source.fromFile(ConfigFactory.load().getString("footerPath")).mkString
-
   val location = ConfigFactory.load().getString("outputFileLocation")
+  val Mdlocation = ConfigFactory.load().getString("MdFileLocation")
   val fileReadObject = new MdFilehandler
 
   /**
@@ -28,20 +28,18 @@ class FileModification {
     val listOfFiles = readListOfFiles()
     val statusList = listOfFiles.map { file =>
       val fileURLContent = scala.io.Source.fromURL(url + file + inputFileExtension).mkString
-      val getFileData: Option[String] = fileReadObject.getFileContent(fileURLContent)
+      val getFileData = fileReadObject.getFileContent(fileURLContent)
       getFileData match {
         case Some(data: String) => val fileData = fileReadObject.ConvertMdExtension(data)
           logger.info("Begin writing [" + file + outputFileExtension + "] at " + location)
-          writeToFile(location + file + outputFileExtension, fileData)
-          logger.info("Successfully written [" + file  + outputFileExtension + "] at " + location)
+          writeHtmlFile(location + file + outputFileExtension, fileData)
+          writeMdFile(Mdlocation + file + inputFileExtension, fileURLContent)
+          logger.info("Successfully written [" + file + outputFileExtension + "] at " + location)
           "Success"
         case None => logger.error(s"$file Conversion failed ")
           "Failure"
       }
     }
-
-    fileReadObject.ConvertReadMeExtension()
-    logger.info("Restored back the ReadMe.md file extension in Installation Guide")
 
     if (statusList.contains("Failure"))
       "Some Files Failed To Convert"
@@ -54,7 +52,7 @@ class FileModification {
     *
     * @return list of string
     */
-  private def readListOfFiles(): List[String] = {
+  def readListOfFiles(): List[String] = {
     val listOfFiles = ConfigFactory.load().getStringList("fileList").asScala.toList
     logger.info(s"List of files : $listOfFiles")
     listOfFiles
@@ -66,10 +64,17 @@ class FileModification {
     * @param path storage location of the file
     * @param data contents of the file
     */
-  private def writeToFile(path: String, data: String): Unit = {
+  def writeHtmlFile(path: String, data: String): Unit = {
     val writer = new PrintWriter(new File(path))
     writer.write(headerContent + data + footerContent)
     writer.close()
   }
+
+  def writeMdFile(path: String, data: String): Unit = {
+    val writer = new PrintWriter(new File(path))
+    writer.write(data)
+    writer.close()
+  }
+
 }
 
